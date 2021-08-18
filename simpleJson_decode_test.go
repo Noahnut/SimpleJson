29be286dbbb08da4de144823262201dc4f66641d
Noahnut <liveinthewide@gmail.com>
2021-08-18 22:18:47 +0800

    Feat: Add the valid array unit test

--- simpleJson_decode_test.go
+++ simpleJson_decode_test.go
@@ -23,4 +23,19 @@ func Test_checkValid(t *testing.T) {
 	if result != nil {
 		t.Fatal(result)
 	}
+
+	testJson = `{"A": [1,2,4]}`
+	result = checkValid([]byte(testJson))
+
+	if result != nil {
+		t.Fatal(result)
+	}
+
+	testJson = `{"A": {"testOne":"testTwo", "E": [1,2,3,4], "F":["1", "3"]}, "B":"C", "num":10}`
+	result = checkValid([]byte(testJson))
+
+	if result != nil {
+		t.Fatal(result)
+	}
+
 }
